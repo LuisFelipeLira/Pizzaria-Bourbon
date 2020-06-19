bd306c84ad3b314fa305b4ea3c6122445a1d4829
luide50 <62894791+luide50@users.noreply.github.com>
2020-06-19 16:32:57 -0300

    .

--- precos.js
+++ precos.js
@@ -1,10 +1,12 @@
 function process(quant){
     var value = parseInt(document.getElementById("quant").value);
     value+=quant;
-    if(value < 1){
-	  document.getElementById("quant").value = 0;}
-	  else{
-	  document.getElementById("quant").value = value;
-	  
+    if(value < 0){
+      document.getElementById("quant").value = 0;
+    }else{
+    document.getElementById("quant").value = value;
     }
+  }
+  function myFunction() {
+    alert("Obrigado, pela compra");
   }
